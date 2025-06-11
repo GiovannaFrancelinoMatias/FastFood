3ab39aaa8413ebd01b8638b972fecebafc7c7d6a
Giovanna <Gi@email.com>
2025-06-11 11:06:30 -0300

    PRODUTO -Iniciada a criação do Produto; -Service, Controler, Repository etc.

---
+++ src/main/java/br/gih/eti/FastFood/Repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.gih.eti.FastFood.Repository;
+
+/**
+ *
+ * @author sesideva
+ */
+public interface ProdutoRepository {
+    
+}
---
+++ src/main/java/br/gih/eti/FastFood/domain/model/Produto.java
@@ -0,0 +1,94 @@
+
+package br.gih.eti.FastFood.domain.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author sesideva
+ */
+public class Produto {
+    
+    private long id;
+    private String nome;
+    private String categoria;
+    private double preco;
+
+    public Produto() {
+    }
+
+    public Produto(long id, String nome, String categoria, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + (int) (this.id ^ (this.id >>> 32));
+        hash = 59 * hash + Objects.hashCode(this.nome);
+        hash = 59 * hash + Objects.hashCode(this.categoria);
+        hash = 59 * hash + (int) (Double.doubleToLongBits(this.preco) ^ (Double.doubleToLongBits(this.preco) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Produto other = (Produto) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.preco) != Double.doubleToLongBits(other.preco)) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return Objects.equals(this.categoria, other.categoria);
+    }
+
+   
+}
